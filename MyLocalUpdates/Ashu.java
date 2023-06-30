0299153bdab806b76d741cf6643709cbb16897e0
Ashupa2el <137591364+Ashupa2el@users.noreply.github.com>
2023-07-01 01:32:17 +0530

    .

---
+++ MyLocalUpdates/Ashu.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Ashu {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Ashutosh Patel\nI am started coding in june 2023\nI choose my first programming lang as Java");
+    }
+
+
+}
